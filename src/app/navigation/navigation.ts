4ae45bea70cb819729bf98cac8ceaba44b8360fe
Block <rm644111@gmail.com>
2018-09-12 20:18:37 -0700

    Design

---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,39 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id   : 'about',
+        title: 'ABOUT',
+        type : 'item',
+        icon : 'alarm',
+        url  : '/pages/coming-soon'
+    },
+    {
+        id   : 'contact',
+        title: 'CONTACT',
+        type : 'item',
+        icon : 'alarm',
+        url  : '/pages/coming-soon'
+    },
+    {
+        id   : 'whitepaper',
+        title: 'WHITEPAPER',
+        type : 'item',
+        icon : 'alarm',
+        url  : '/pages/coming-soon'
+    },
+    {
+        id   : 'source-code',
+        title: 'SOURCE CODE',
+        type : 'item',
+        icon : 'alarm',
+        url  : '/pages/coming-soon'
+    },
+    {
+        id   : 'blog',
+        title: 'BLOG',
+        type : 'item',
+        icon : 'alarm',
+        url  : '/pages/coming-soon'
+    }
+];
